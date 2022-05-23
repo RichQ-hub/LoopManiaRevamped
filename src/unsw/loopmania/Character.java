afd66d79de572f0975e586cb92969ed561fa2b21
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2022-05-23 18:50:49 +1000

    Added character stats

--- src/unsw/loopmania/Character.java
+++ src/unsw/loopmania/Character.java
@@ -1,12 +1,51 @@
 package unsw.loopmania;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 /**
  * represents the main character in the backend of the game world
  */
 public class Character extends MovingEntity {
-    // TODO = potentially implement relationships between this class and other classes
+    IntegerProperty gold;
+    IntegerProperty exp;
+    
     public Character(PathPosition position) {
         super(position);
+        this.gold = new SimpleIntegerProperty();
+        this.exp = new SimpleIntegerProperty();
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //                           Property Getters                                   //
+    //////////////////////////////////////////////////////////////////////////////////
+
+    public IntegerProperty getGoldProperty() {
+        return gold;
+    }
+
+    public IntegerProperty getExpProperty() {
+        return exp;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //                           Getters and Setters                                //
+    //////////////////////////////////////////////////////////////////////////////////
+
+    public int getGold() {
+        return gold.get();
+    }
+
+    public void setGold(int gold) {
+        this.gold.set(gold);;
+    }
+
+    public int getExp() {
+        return exp.get();
+    }
+
+    public void setExp(int exp) {
+        this.exp.set(exp);
     }
     
 }
